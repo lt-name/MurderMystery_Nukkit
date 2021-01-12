be898fed2e0a886e7f1a1ad0f9595c4745ea1712
lt_name <572413378@qq.com>
2021-01-12 20:53:00 +0800

    Stuff

--- src/main/java/cn/lanink/murdermystery/listener/assassin/AssassinDamageListener.java
+++ src/main/java/cn/lanink/murdermystery/listener/assassin/AssassinDamageListener.java
@@ -2,16 +2,11 @@ package cn.lanink.murdermystery.listener.assassin;
 
 import cn.lanink.murdermystery.listener.BaseMurderMysteryListener;
 import cn.lanink.murdermystery.room.assassin.AssassinModeRoom;
-import cn.lanink.murdermystery.room.base.BaseRoom;
-import cn.lanink.murdermystery.utils.Tools;
 import cn.nukkit.Player;
-import cn.nukkit.entity.Entity;
 import cn.nukkit.event.EventHandler;
-import cn.nukkit.event.entity.EntityDamageByChildEntityEvent;
 import cn.nukkit.event.entity.EntityDamageByEntityEvent;
-import cn.nukkit.level.Sound;
+import cn.nukkit.item.Item;
 import cn.nukkit.nbt.tag.CompoundTag;
-import cn.nukkit.potion.Effect;
 
 /**
  * @author lt_name
@@ -30,39 +25,19 @@ public class AssassinDamageListener extends BaseMurderMysteryListener<AssassinMo
             if (damager == null || player == null) {
                 return;
             }
-            BaseRoom room = this.getListenerRooms().get(damager.getLevel().getFolderName());
+            AssassinModeRoom room = this.getListenerRooms().get(damager.getLevel().getFolderName());
             if (room == null) {
                 return;
             }
             event.setCancelled(true);
-            if (event instanceof EntityDamageByChildEntityEvent) {
-                EntityDamageByChildEntityEvent event1 = (EntityDamageByChildEntityEvent) event;
-                damager = (Player) event1.getDamager();
-                player = (Player) event1.getEntity();
-                Entity child = event1.getChild();
-                if (child == null || child.namedTag == null) {
-                    return;
-                }
-                if (child.namedTag.getBoolean("isMurderItem")) {
-                    if (child.namedTag.getInt("MurderType") == 20) {
-                        room.playerDamage(damager, player);
-                    }else if (child.namedTag.getInt("MurderType") == 23) {
-                        Tools.playSound(player, Sound.RANDOM_ANVIL_LAND);
-                        player.sendMessage(this.murderMystery.getLanguage(player).translateString("damageSnowball"));
-                        Effect effect = Effect.getEffect(2);
-                        effect.setAmplifier(2);
-                        effect.setDuration(40);
-                        player.addEffect(effect);
-                    }
-                }
-            }else {
-                if (room.isPlaying(damager) && room.getPlayers(damager) == 3 &&
-                        room.isPlaying(player) && room.getPlayers(player) != 0) {
-                    if (damager.getInventory().getItemInHand() != null) {
-                        CompoundTag tag = damager.getInventory().getItemInHand().getNamedTag();
-                        if (tag != null && tag.getBoolean("isMurderItem") && tag.getInt("MurderType") == 2) {
-                            room.playerDamage(damager, player);
-                        }
+            if (room.isPlaying(damager) && room.isPlaying(player) && room.targetMap.get(damager) == player) {
+                Item item = damager.getInventory().getItemInHand();
+                if (item != null && item.hasCompoundTag()) {
+                    CompoundTag tag = item.getNamedTag();
+                    if (tag.getBoolean("isMurderItem") && tag.getInt("MurderType") == 2) {
+                        //TODO 重新分配目标
+
+                        room.playerDeath(player);
                     }
                 }
             }
