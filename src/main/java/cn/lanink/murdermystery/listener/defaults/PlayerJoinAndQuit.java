2c1a80425a1fa40b2a41c26c219ad3fa2391a9ed
lt_name <572413378@qq.com>
2021-02-26 16:22:25 +0800

    优化

--- src/main/java/cn/lanink/murdermystery/listener/defaults/PlayerJoinAndQuit.java
+++ src/main/java/cn/lanink/murdermystery/listener/defaults/PlayerJoinAndQuit.java
@@ -9,6 +9,7 @@ import cn.lanink.murdermystery.utils.Tools;
 import cn.nukkit.Player;
 import cn.nukkit.Server;
 import cn.nukkit.event.EventHandler;
+import cn.nukkit.event.EventPriority;
 import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerJoinEvent;
 import cn.nukkit.event.player.PlayerQuitEvent;
@@ -66,7 +67,7 @@ public class PlayerJoinAndQuit implements Listener {
 
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onPlayerTp(PlayerTeleportEvent event) {
         Player player = event.getPlayer();
         String fromLevel = event.getFrom().getLevel() == null ? null : event.getFrom().getLevel().getFolderName();
